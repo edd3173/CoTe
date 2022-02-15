536a815974f7ebba7838170266aacdfdaf03cf36
edd3173 <stargazers737@gmail.com>
2022-02-15 22:19:56 +0900

    2nd_Loop, ~ BFS/DFS

--- Graph/Graph_2.py
+++ Graph/Graph_2.py
@@ -1,33 +1,69 @@
-""" Ice Cream """
+from collections import deque
 
-n,m = list(map(int,input().split()))
 
+"""
+Added BFS answer code.
+"""
+
+
+n,m = map(int,input().split())
+move_set=[(0,+1),(0,-1),(+1,0),(-1,0)]
 graph=[]
-for i in range(n):
-  graph.append(list(map(int,input()))) # no split. 1 0 1 1 이 아니라, 1011로 입력받으니까
+#cnt=0
+for _ in range(n):
+  graph.append(list(map(int,input())))
 
-visited = [[False]* m for _ in range(n)]
-cnt=0
-
-def DFS(x,y): # Return type 정의 없이 
-  if x<0 or y<0 or x>=n or y>=m:
-    return False # Return 가능
-  if graph[x][y] == 0: # At start, return False
-    #Now we search. How?
-    graph[x][y]=1
-    DFS(x+1,y)
-    DFS(x-1,y)
-    DFS(x,y+1)
-    DFS(x,y-1)
-    # 처음 dfs에 대해서만 True를 리턴하게 한다.
-    return True # At last, return True. WHEN all zeros are gone, and switched to one. (iterated all chunk)
+"""
+def dfs(px,py):
+  if px<0 or px>=n or py<0 or py>=m: # blocked
+    return False
+  if graph[px][py]==0: # land, or unvisited
+    graph[px][py]=1 # mark visited (by changing land to water)
+    dfs(px+1,py)
+    dfs(px-1,py)
+    dfs(px,py+1)
+    dfs(px,py-1)
+    return True # Last visit in chunk, return true
   return False
+"""
 
+def bfs(px,py):
+  if graph[px][py] == 1:
+    #print("Wrong Input!")
+    return False
+
+  q=deque([(px,py)])
+  graph[px][py]=1 # mark visited
+  print("(%d, %d)",px,py)
+  while q:
+    v = q.popleft()
+    px=v[0]; py=v[1]
+    for move in move_set:
+      nx=px+move[0]; ny=py+move[1]
+      if nx<0 or nx>=n or ny<0 or ny>=m :
+        continue
+      if graph[nx][ny] == 0:
+        graph[nx][ny]=1
+        q.append((nx,ny))
+  return True
+    
+cnt=0
 for i in range(n):
   for j in range(m):
-    if DFS(i,j)==True:
-      cnt+=1
+    if bfs(i,j)==True: cnt+=1
 
 print(cnt)
 
 
+
+      
+    
+
+
+"""
+for i in range(n):
+  for j in range(m):
+    if dfs(i,j)==True: cnt+=1
+"""
+
+    
