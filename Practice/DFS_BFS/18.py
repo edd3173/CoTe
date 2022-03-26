c69d3970427f054637e819b3b5b795eadbf4dc74
edd3173 <stargazers737@gmail.com>
2022-03-26 09:38:27 +0900

    Fix Q18

--- Practice/DFS_BFS/18.py
+++ Practice/DFS_BFS/18.py
@@ -1,5 +1,52 @@
-N=int(input())
-numbers=list(map(int,input().split()))
-# +,-.x,//
-oper= list(map(int,input().split()))
 
+
+def isBalanced(p):
+    leftCount = p.count('(')
+    rightCount = p.count(')')
+    if leftCount==rightCount: return True
+    else: return False
+
+def isCorrect(p):
+    Count=0
+    for i in range(len(p)):
+        if p[i]=='(':
+            Count+=1
+        if p[i]==')':
+            if Count==0:
+                return False
+            Count-=1
+    if Count !=0: # need not, but good job.
+        return False
+    return True
+
+def findBalancedIdx(p):
+    count=0
+    for i in range(len(p)):
+        if p[i]=='(': count+=1
+        if p[i]==')': count-=1
+        if count==0: return i
+
+
+def solution(p):
+    if not p: return p
+    idx = findBalancedIdx(p)
+    u = p[:idx+1]
+    v = p[idx+1:]
+    if isCorrect(u): 
+        return u+solution(v)
+    else:
+        temp="("+solution(v)+')'
+        u=list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        temp+="".join(u)
+        return temp
+
+#p = "(()())()"
+#p = ")("
+p = "()))((()"
+result = solution(p)
+print(result)
