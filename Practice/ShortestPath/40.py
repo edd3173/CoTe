48de4ce4e993f10d6d4accf9eb3a2243d9159ba3
edd3173 <stargazers737@gmail.com>
2022-04-25 17:45:29 +0900

    ~Q40

--- Practice/ShortestPath/40.py
+++ Practice/ShortestPath/40.py
@@ -65,11 +65,13 @@ def bfs(start):
 
 bfs(1)
 print(distance)
-distance[0]=-1
-cnt=0
-maxVal = max(distance)
-maxIdx = distance.index(maxVal)
-for d in distance:
-    if d == maxVal: cnt+=1
 
-print(maxIdx,maxVal,cnt)
+
+maxVal=-1; maxNode=-1; cnt=0
+
+maxVal = max(distance[1:])
+maxNode = distance.index(maxVal)
+cnt = distance.count(maxVal)
+
+
+print(maxNode,maxVal,cnt)
