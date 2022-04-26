21e742245ef02158019af3fdf5a419761c95a5e5
edd3173 <stargazers737@gmail.com>
2022-04-26 23:51:40 +0900

    yey

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-import heapq
-
-n=int(input())
-h=[]
-for _ in range(n):
-    heapq.heappush(h,int(input()))
-
-ans=0
-
-while len(h)!=1:
-    first = heapq.heappop(h)
-    second = heapq.heappop(h)
-    ans += first+second
-    heapq.heappush(h,first+second)
-
-print(ans)
+n,m,k = map(int,input().split())
