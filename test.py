dfadb0d21d10f136d0a4e2e25c5856fab5a86215
edd3173 <stargazers737@gmail.com>
2022-04-29 00:25:36 +0900

    04.28.

--- test.py
+++ test.py
@@ -1,13 +1,36 @@
-def binary_search(arr,target,start,end):
-    while start<=end:
-        mid = (start+end) // 2
-        if arr[mid]==target:
-            return mid
-        elif arr[mid]<target:
-            start=mid+1
+def solution(new_id):
+    answer = ''
+    
+    #step1
+    new_id = new_id.lower()
+    
+    #step2
+    for ch in new_id:
+        if ch.isalpha() or ch.isdigit() or ch=='-' or ch=='_' or ch=='.':
+            continue
         else:
-            end=mid-1
-    return None
+            new_id = new_id.replace(ch,"")
+    print(new_id)
+    
+    #step3
+    targets=[]
+    for i in range(len(new_id)-1):
+        if new_id[i]=='.' and new_id[i+1]=='.':
+            targets.append(i)
+    
+    for i in range(len(targets)):
+        new_id = new_id.replace("..",".")
+    print(new_id)
+    
+    #step4
+    if new_id[0]=='.':
+        new_id = new_id[1:]
+    #print(new_id[-1])        
+    
+    print("["+new_id+"]")
+    print(new_id[-1])
+    
+    return answer
 
-N,M = map(int,input().split())
-arr = list(map(int,input()))
+myStr="...!@BaT#*..y.abcdefghijklm"
+solution(myStr)
