ce71bd5d9ffd9cd791d4d53ffc98ce25206f723a
edd3173 <stargazers737@gmail.com>
2022-05-04 00:27:04 +0900

    HARD : 18870

--- BOJ/Sort/[HARD]18870.py
+++ BOJ/Sort/[HARD]18870.py
@@ -1,3 +1,8 @@
+"""
+HARD!
+"""
+
+
 import sys
 
 input = sys.stdin.readline
@@ -7,9 +12,9 @@ arr = list(map(int, input().split()))
 arr2 = sorted(list(set(arr)))
 myDict = { arr2[i]: i for i in range(len(arr2))}
 
-print("arr1:",arr)
-print("arr2:",arr2)
-print("myDict:",myDict)
+# print("arr1:",arr)
+# print("arr2:",arr2)
+# print("myDict:",myDict)
 
 for i in arr:
     print(myDict[i],end=' ')
